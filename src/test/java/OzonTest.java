ab74f7477330549de868469fe11725c3a8cdec6a
Demidov, Yevgeny <Yevgeny.Demidov@nedra.digital>
2022-08-23 22:46:00 +0300

    test

--- src/test/java/OzonTest.java
+++ src/test/java/OzonTest.java
@@ -1,9 +1,11 @@
 import com.codeborne.selenide.Selenide;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class OzonTest {
     @Test
     public void openPage(){
         Selenide.open("https://www.ozon.ru/");
+        Assert.assertEquals(23,24);
     }
 }
