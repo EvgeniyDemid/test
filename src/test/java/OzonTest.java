7191349b4d111f0cd57658e9840ee10e9f9e0ae2
Demidov, Yevgeny <Yevgeny.Demidov@nedra.digital>
2022-11-15 09:54:50 +0300

    test

--- src/test/java/OzonTest.java
+++ src/test/java/OzonTest.java
@@ -1,11 +1,17 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
 
 public class OzonTest {
     @Test
     public void openPage(){
         Selenide.open("https://www.ozon.ru/");
-        Assert.assertEquals(23,24);
+        Selenide.sleep(1000);
+        $(byXpath("//a[contains(text(),'Электроника')]")).shouldBe(Condition.visible);
+        $(byXpath("//a[contains(text(),'Акции')]")).shouldBe(Condition.visible);
     }
 }
